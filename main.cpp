73c0cc11fc9bd3ff6cb3bee71390f04d851377db
alxr301 <renderosa394@gmail.com>
2024-10-23 12:06:36 -0700

    hi

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include <iomanip>
+#include <iostream>
+using namespace std; 
+
 #include "main.hpp"
 
 // Do not change this file.
--- main.hpp
+++ main.hpp
@@ -1,8 +1,46 @@
+#include <iomanip>
+#include <iostream>
+using namespace std; 
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int &begin, int &end){
+    do{
+        cout << "Input Two Values: ";
+        cin >> begin >> end;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+    }while(begin >= end); 
+
+}
+
+int getNextPrime(int begin){
+    int i, isprime; 
+    for(i = begin; i++;){
+        isprime = 1; 
+        for(int j = 2; j <= (i/2); j++){
+            if(i%j == 0){
+                isprime = 0; 
+                break;
+            }
+        }
+        if(isprime == 1){
+            break; 
+        }
+    }
+    return i; 
+}
+
+int getPrevPrime(int end){
+    int i, isprime;
+    for(i = end; i--;){
+        isprime = 1;
+        for(int j = 2; j <= (i/2); j++){
+            if(i%j == 0){
+                isprime = 0;
+                break;
+            }
+        }
+        if(isprime == 1){
+            break;
+        }
+    }
+    return i; 
+}
